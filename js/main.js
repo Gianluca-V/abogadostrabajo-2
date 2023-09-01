d9c284e8c4e43cd2b5a56e1d6eeaf41eabaf06c1
Gianluca-V <vespe.gianluca@gmail.com>
2023-09-01 13:39:28 -0300

    changed color palete and added home section

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const accordionItems = document.querySelectorAll(".working-areas__div");
-  
-    accordionItems.forEach((item) => {
-      const descriptionElement = item.querySelector(".working-areas__description");
-  
-      item.querySelector(".working-areas__toggle").addEventListener("click", () => {
-        // Toggle the active class on the description element
-        item.classList.toggle("active");
-        descriptionElement.classList.toggle("active");
-        item.querySelector("#hammer").classList.remove("hammer");
-        setTimeout(()=>{item.querySelector("#hammer").classList.add("hammer");},1);
-        
-
-  
-        // Collapse other open items
-        accordionItems.forEach((otherItem) => {
-          if (otherItem !== item && otherItem.classList.contains("active")) {
-            otherItem.querySelector(".working-areas__description").classList.remove("active");
-            otherItem.classList.remove("active");
-          }
-        });
-      });
-    });
 
     const navLinks = document.querySelectorAll(".nav__li a");
     navLinks.forEach(link => {
@@ -31,4 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
       })
   })
 
+  window.onresize = ()=>{
+    document.querySelector(".home").style.paddingTop = (20 + document.querySelector(".header").offsetHeight) + 'px';
+  };
 });
